71415f8e7af52ca21fe2076d872ba1261288f673
Chris Hartwick <unholycow@gmail.com>
2015-11-14 20:01:14 -0600

    Work done on initiative tracker.
    Still need to get the sorting logic working.

--- js/init.js
+++ js/init.js
@@ -1,5 +1,30 @@
 $(document).ready(function(){
 
-  $('.remove').click
+  // Remove button removes the selected slot.
+  $('.remove').click(function(){
+    $(this).closest('.slot').remove();
+  });
+
+  // Begin a new round: update the counter, remove and reset effects.
+  $('#end-turn').click(function(){
+
+    $('#round-count').html(function(i, val) { return +val+1 });
+
+    $('.slot').each(function(){
+      $(this).removeClass('finished active');
+      $('.slot:first-child').addClass('active');
+    });
+
+  });
+
+  // Add and order each new combatant.
+  $('.add-pc').click(function(){
+
+    var success = $('#success-roll').val();
+    var advantage = $('#advantage-roll').val();
+
+    $('.tracker-list').
+
+  });
 
 });
