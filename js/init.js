0461972b252c20f2649438fcc480c28014a9cbf8
Chris Hartwick <christopher.hartwick@gmail.com>
2015-11-16 20:28:15 -0600

    11/15 session updates and expansion of DM sections.

--- js/init.js
+++ js/init.js
@@ -1,30 +1,67 @@
 $(document).ready(function(){
 
-  // Remove button removes the selected slot.
-  $('.remove').click(function(){
-    $(this).closest('.slot').remove();
-  });
-
   // Begin a new round: update the counter, remove and reset effects.
-  $('#end-turn').click(function(){
+  $('#end-round').click(function(){
 
     $('#round-count').html(function(i, val) { return +val+1 });
 
     $('.slot').each(function(){
-      $(this).removeClass('finished active');
-      $('.slot:first-child').addClass('active');
+      $(this).removeClass('finished active-slot');
+      $('.slot:first-child').addClass('active-slot');
     });
 
   });
 
-  // Add and order each new combatant.
+  // Add new combatants, probably 3 different funtions.
   $('.add-pc').click(function(){
 
     var success = $('#success-roll').val();
     var advantage = $('#advantage-roll').val();
 
-    $('.tracker-list').
+    $('.tracker-list').append('<div class="slot"></div>');
+    $('.slot:last-child').append('<div class="field player"><span>PC</span></div>');
+     $('.slot:last-child').append('<div class="field success-roll"><span>'+ success +'</span><div class="successBig"></div></div>');
+    $('.slot:last-child').append('<div class="field advantage-roll"><span>'+ advantage +'</span><div class="advantageBig"></div></div>');
+    $('.slot:last-child').append('<div class="field"><div class="remove">X</div></div>');
 
+    $('#success-roll').val("");
+    $('#advantage-roll').val("");
+
+  });
+  $('.add-ally').click(function(){
+
+    var success = $('#success-roll').val();
+    var advantage = $('#advantage-roll').val();
+
+    $('.tracker-list').append('<div class="slot"></div>');
+    $('.slot:last-child').append('<div class="field ally"><span>NPC</span></div>');
+    $('.slot:last-child').append('<div class="field success-roll"><span>'+ success +'</span><div class="successBig"></div></div>');
+    $('.slot:last-child').append('<div class="field advantage-roll"><span>'+ advantage +'</span><div class="advantageBig"></div></div>');
+    $('.slot:last-child').append('<div class="field"><div class="remove">X</div></div>');
+
+    $('#success-roll').val("");
+    $('#advantage-roll').val("");
+
+  });
+  $('.add-enemy').click(function(){
+
+    var success = $('#success-roll').val();
+    var advantage = $('#advantage-roll').val();
+
+    $('.tracker-list').append('<div class="slot"></div>');
+    $('.slot:last-child').append('<div class="field enemy"><span>NPC</span></div>');
+    $('.slot:last-child').append('<div class="field success-roll"><span>'+ success +'</span><div class="successBig"></div></div>');
+    $('.slot:last-child').append('<div class="field advantage-roll"><span>'+ advantage +'</span><div class="advantageBig"></div></div>');
+    $('.slot:last-child').append('<div class="field"><div class="remove">X</div></div>');
+
+    $('#success-roll').val("");
+    $('#advantage-roll').val("");
+
+  });
+
+  // Remove button removes the selected slot.
+  $(document).on("click", ".remove", function() {
+    $(this).parent().parent().remove();
   });
 
 });
