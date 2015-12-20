e95a360b12ace4977834eba02a70ae9b7694b2b2
UnholyCow <christopher.hartwick@gmail.com>
2015-12-19 23:47:34 -0600

    More additions for 12/20. STAR WARS!

--- js/init.js
+++ js/init.js
@@ -1,171 +1,56 @@
-$(document).ready(function(){
-
-  //Enables sorting for the init list.
-  $(function() {
-    $( "#sortable" ).sortable();
-    $( "#sortable" ).disableSelection();
-  });
-
-  // Begin combat resets round counter and selects the first slot.
-  $(document).on("click", "#begin-combat", function() {
-    $('#round-count').html(function(i, val) { return 1 });
-
-    $('.slot').each(function(){
-      $(this).removeClass('finished active-slot');
-      $('.slot:first-child').addClass('active-slot');
-    });
-  });
-
-  //End combat removes all slot modifiers and resets round count.
-  $(document).on("click", "#end-combat", function() {
-    $('#round-count').html(function(i, val) { return 1 });
-
-    $('.slot').each(function(){
-      $(this).removeClass('finished active-slot');
-    });
-  });
-
-  // End turn finishes current slot, activates next slot.
-  $(document).on("click", "#end-turn", function(){
-  	$('li.active-slot').addClass('finished').next().addClass('active-slot');
-  });
-
-  // Begin a new round: update the counter, remove and reset effects.
- $(document).on("click", "#end-round", function(){
-
-    $('#round-count').html(function(i, val) { return +val+1 });
-
-    $('.slot').each(function(){
-      $(this).removeClass('finished active-slot');
-      $('.slot:first-child').addClass('active-slot');
-    });
-
-  });
-
- // Resets for after adding a combatant.
-	var reset = function () {
-  	$('#success-roll').val("");
-    $('#advantage-roll').val("");
-    $('#triumph-roll').val("");
-    $('#char-name').val("");
-    $('#wound-thr').val("");
-    $('#strain-thr').val("");
-  };
-
-  //Function to create the common bits of an initiative slot.
-  var fill = function () {
-  	var success = $('#success-roll').val();
-    var advantage = $('#advantage-roll').val();
-    var triumph = $('#triumph-roll').val();
-
-    if (success > 0 || advantage > 0 || triumph > 0) {
-      $('.slot:last-child').append('<div class="field success-roll"><span>'+ success +'</span><span class="eote su init"></span></div>');
-      $('.slot:last-child').append('<div class="field advantage-roll"><span>'+ advantage +'</span><span class="eote ad init"></span></div>');
-      $('.slot:last-child').append('<div class="field triumph-roll"><span>'+ triumph +'</span><span class="eote tr init"></span></div>');
-      $('.slot:last-child').append('<div class="field"><div class="remove">X</div></div>');
-    } else {
-      return false;
-    }
-  };
+$(document).ready(function () {
   
-  //Function to create a monitor entry.
-  var monitor = function () {
-    var name = $('#char-name').val();
-    var wounds = $('#wound-thr').val();
-    var strain = $('#strain-thr').val();
-    
-    if (name.length > 0) {
-      
-      $("#monitor-container").append("<div class='char-block working'></div>");
-      $("div.working").append("<h3>" + name + "</h3>")
-      
-      if (wounds.length > 0) {
-        $("div.working").append(
-          "<div class='stat-small-wide'>"+
-            "<div class='tag-small clear-wounds'>Wounds / Threshold</div>"+
-            "<div class='value-small adj-wounds'><span class='wounds'>0</span> / <span class='wound-thr'>"+wounds+"</span></div>"+
-          "</div> ")
-      }
-      
-      if (strain.length > 0) {
-        $("div.working").append(
-          "<div class='stat-small-wide'>"+
-            "<div class='tag-small clear-strain'>Strain / Threshold</div>"+
-            "<div class='value-small adj-strain'><span class='strain'>0</span> / <span class='strain-thr'>"+strain+"</span></div>"+
-          "</div> ")
-      }
-      
-      $("div.working").append(
-        "<div class='stat-small'>"+
-          "<div class='tag-small clear-boost'><span class='eote bo2'></span></div>"+
-          "<div class='value-small adj-boost'>0</div>"+
-        "</div> ")
-        $("div.working").append(
-        "<div class='stat-small'>"+
-          "<div class='tag-small clear-setback'><span class='eote se2'></span></div>"+
-          "<div class='value-small adj-setback'>0</div>"+
-        "</div>")
-      
-      $("div.working").removeClass("working");
-    } else {
-      return false;
-    }
-  };
-
-  // Add new combatants.
-  $('.add-pc').click(function(){
-    var success = $('#success-roll').val();
-    var advantage = $('#advantage-roll').val();
-    var triumph = $('#triumph-roll').val();
-    
-    if (success > 0 || advantage > 0 || triumph > 0) {
-      $('.tracker-list').append('<li class="slot"></div>');
-      $('.slot:last-child').append('<div class="field player"><span>PC</span></div>');
-    }
-
-    fill();
-    monitor();
-    reset();
+  /* ======================================== */
+  /* ========== INITIATIVE TRACKER ========== */
+  /* ======================================== */
+  
+  //Enables sorting for the initiative tracker.
+  $(function () {
+    $("#sortable").sortable();
+    $("#sortable").disableSelection();
   });
-
-  $('.add-ally').click(function(){
-    var success = $('#success-roll').val();
-    var advantage = $('#advantage-roll').val();
-    var triumph = $('#triumph-roll').val();
-    
-    if (success > 0 || advantage > 0 || triumph > 0) {
-      $('.tracker-list').append('<li class="slot"></div>');
-      $('.slot:last-child').append('<div class="field ally"><span>NPC</span></div>');
-    }
-
-    fill();
-    monitor();
-    reset();
-  });
-
-  $('.add-enemy').click(function(){
-    var success = $('#success-roll').val();
-    var advantage = $('#advantage-roll').val();
-    var triumph = $('#triumph-roll').val();
-    
-    if (success > 0 || advantage > 0 || triumph > 0) {
-      $('.tracker-list').append('<li class="slot"></div>');
-      $('.slot:last-child').append('<div class="field enemy"><span>NPC</span></div>');
-    }
-
-    fill();
-    monitor();
-    reset();
-  });
-
-  // Remove button removes the selected slot.
-  $(document).on("click", ".remove", function() {
-    $(this).parent().parent().remove();
-  });
-
-  $(document).on("click", "#clear", function() {
-    $(".tracker-list").children('li').remove();
-    $("#monitor-container").empty();
-  });
+  
+  //Declare the actual Initiative tracker.
+  function Tracker() {
+    this.slots = [];
+    this.activeIndex = 0;
+    this.rounds = 1;
+  }
+  
+  Tracker.prototype.add = function(slot) {
+    this.slots.push(slot);
+  }
+  
+  Tracker.prototype.nextTurn = function() {
+    this.activeIndex++;
+  }
+  
+  Tracker.prototype.nextRound = function() {
+    this.activeIndex = 0;
+  }
+  
+  var tracker = new Tracker();
+  
+  //Declare Slots for the initiative tracker.
+  function Slot(type, success, advantage, triumph) {
+    this.type = type;
+    this.success = success;
+    this.advantage = advantage;
+    this.triumph = triumph;
+    this.isActive = false;
+  }
+  
+  /* ======================================== */
+  /* ========== CHARACTER MONITOR =========== */
+  /* ======================================== */
+  
+  //Declare Characters for the character monitor.
+  function Character(name, woundThr, strainThr) {
+    this.name = name;
+    this.woundThr = woundThr;
+    this.strainThr = strainThr;
+    this.boost = 0;
+    this.setback = 0;
+  }
 
 });
--- js/main.js
+++ js/main.js
@@ -1,114 +1,114 @@
-$(document).ready(function(){
+$(document).ready(function () {
 	
-	//Check to see if the window is top if not then display button
-	$(window).scroll(function(){
-		if ($(this).scrollTop() > 100) {
-			$('.back-to-top').fadeIn();
-		} else {
-			$('.back-to-top').fadeOut();
-		}
+  //Check to see if the window is top if not then display button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn();
+    } else {
+      $('.back-to-top').fadeOut();
+    }
 
-		//Also sets top for mobile navigation, disabled unless needed
-		// $scroll = window.pageYOffset;
-		// $('nav').css({ top: $scroll });
-	});
+    //Also sets top for mobile navigation, disabled unless needed
+    // $scroll = window.pageYOffset;
+    // $('nav').css({ top: $scroll });
+  });
 	
-	//Click event to scroll to top
-	$('.back-to-top').click(function(){
-		$('html, body').animate({scrollTop : 0},200);
-		return false;
-	});
+  //Click event to scroll to top
+  $('.back-to-top').click(function () {
+    $('html, body').animate({ scrollTop: 0 }, 200);
+    return false;
+  });
 
-	//Mobile CSS menu
-	$('#hamburger').on('touchstart click', function(e) {
+  //Mobile CSS menu
+  $('#hamburger').on('touchstart click', function (e) {
 
-		e.preventDefault();
+    e.preventDefault();
 
-		//Declare menu to clean up code
-		var $menu = $('.container, header'),
-			$scroll = window.pageYOffset;
+    //Declare menu to clean up code
+    var $menu = $('.container, header'),
+      $scroll = window.pageYOffset;
 
-		//Define the animation type on click
-		$menu.addClass('animate');
-		$('nav').css({ top: $scroll });
+    //Define the animation type on click
+    $menu.addClass('animate');
+    $('nav').css({ top: $scroll });
 
-		//Transition compatability
-      	transitionEnd = 'transitionend webkitTransitionEnd otransitionend MSTransitionEnd';
+    //Transition compatability
+    transitionEnd = 'transitionend webkitTransitionEnd otransitionend MSTransitionEnd';
 
-		//Check if the menu is already visible and animate in the proper direction
-		if ( $menu.hasClass('nav-visible') ) {
-			$menu.addClass('close-nav');
+    //Check if the menu is already visible and animate in the proper direction
+    if ($menu.hasClass('nav-visible')) {
+      $menu.addClass('close-nav');
 
-		} else {
-			$menu.addClass('open-nav');
-		}
+    } else {
+      $menu.addClass('open-nav');
+    }
 
-		//Clean up animations and toggle the nav being visible or not
-		$menu.on( transitionEnd, function() {
-			$menu
-				.removeClass('animate close-nav open-nav')
-				.toggleClass('nav-visible');
+    //Clean up animations and toggle the nav being visible or not
+    $menu.on(transitionEnd, function () {
+      $menu
+        .removeClass('animate close-nav open-nav')
+        .toggleClass('nav-visible');
 
-		$menu.off( transitionEnd );
-		} );
-	} );
+      $menu.off(transitionEnd);
+    });
+  });
 
-	/* PC site navigation =======================  */
-	//Open the dropdown menus.
-	$('.toggle-groups-nav').mouseenter(function(e) {
-		e.preventDefault();
-		$(this).next('ul').show();
-		if ($('#rules-nav').is(':visible')) {
-			$('#rules-nav').hide();
-		}
-	});
+  /* PC site navigation =======================  */
+  //Open the dropdown menus.
+  $('.toggle-groups-nav').mouseenter(function (e) {
+    e.preventDefault();
+    $(this).next('ul').show();
+    if ($('#rules-nav').is(':visible')) {
+      $('#rules-nav').hide();
+    }
+  });
 
-	$('.toggle-rules-nav').mouseenter(function(e) {
-		e.preventDefault();
-		$(this).next('ul').show();
-		if ($('#groups-nav').is(':visible')) {
-			$('#groups-nav').hide();
-		}
-	});
+  $('.toggle-rules-nav').mouseenter(function (e) {
+    e.preventDefault();
+    $(this).next('ul').show();
+    if ($('#groups-nav').is(':visible')) {
+      $('#groups-nav').hide();
+    }
+  });
 
-	//Close the dropdown menus.
-	$('#groups-nav').mouseleave(function(e) {
-		$('#groups-nav').hide();
-	});
+  //Close the dropdown menus.
+  $('#groups-nav').mouseleave(function (e) {
+    $('#groups-nav').hide();
+  });
 
-	$('#rules-nav').mouseleave(function(e) {
-		$('#rules-nav').hide();
-	});
+  $('#rules-nav').mouseleave(function (e) {
+    $('#rules-nav').hide();
+  });
 	
-	/* DM site navigation ====================  */
-	//Open the dropdown menus.
-	$('.toggle-adv-nav').mouseenter(function(e) {
-		e.preventDefault();
-		$(this).next('ul').show();
-	});
+  /* DM site navigation ====================  */
+  //Open the dropdown menus.
+  $('.toggle-adv-nav').mouseenter(function (e) {
+    e.preventDefault();
+    $(this).next('ul').show();
+  });
 
-	//Close the dropdown menus.
-	$('#adv-nav').mouseleave(function(e) {
-		$('#adv-nav').hide();
-	});
+  //Close the dropdown menus.
+  $('#adv-nav').mouseleave(function (e) {
+    $('#adv-nav').hide();
+  });
 
-	//Mobile nav including prepending landing pages for players and rules.
-	$('.toggle-groups-nav').bind('touchstart', function(e) {
-		e.preventDefault();
-		if ($('#groups-nav li').length == 6) {
-			$('#groups-nav').prepend('<li><a href="http://www.unholycow.com/swrpg/groups/monarch/overview.html">Overview</a></li>');
-		}
-		$('#rules-nav').hide();
-		$('#groups-nav').toggle();
-	});
+  //Mobile nav including prepending landing pages for players and rules.
+  $('.toggle-groups-nav').bind('touchstart', function (e) {
+    e.preventDefault();
+    if ($('#groups-nav li').length == 6) {
+      $('#groups-nav').prepend('<li><a href="http://www.unholycow.com/swrpg/groups/monarch/overview.html">Overview</a></li>');
+    }
+    $('#rules-nav').hide();
+    $('#groups-nav').toggle();
+  });
 
-	$('.toggle-rules-nav').bind('touchstart', function(e) {
-		e.preventDefault();
-		if ($('#rules-nav li').length == 3) {
-			$('#rules-nav').prepend('<li><a href="http://www.unholycow.com/swrpg/rules.html">Basics</a></li>');
-		}
-		$('#groups-nav').hide();
-		$('#rules-nav').toggle();
-	});
+  $('.toggle-rules-nav').bind('touchstart', function (e) {
+    e.preventDefault();
+    if ($('#rules-nav li').length == 3) {
+      $('#rules-nav').prepend('<li><a href="http://www.unholycow.com/swrpg/rules.html">Basics</a></li>');
+    }
+    $('#groups-nav').hide();
+    $('#rules-nav').toggle();
+  });
 
 });
