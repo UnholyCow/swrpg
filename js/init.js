c1b69d4b62eca374e39ebe465df24c88608f0c16
UnholyCow <christopher.hartwick@gmail.com>
2015-12-26 22:16:04 -0600

    First combat tracker feature complete. Updates for 12/27: Sessions & crawl.

--- js/init.js
+++ js/init.js
@@ -21,6 +21,13 @@ function Slot(type, success, advantage, triumph) {
 
 var init = new Tracker();
 
+//Declare Characters for the character monitor.
+function Monitor() {
+  this.chars = [];
+}
+
+var display = new Monitor();
+
 $(document).ready(function () {
   
   /* ======================================== */
@@ -34,10 +41,13 @@ $(document).ready(function () {
   });
   
   //Reset function.
-  var reset = function () {
+  var resetInit = function () {
     $('#success-roll').val("");
     $('#advantage-roll').val("");
     $('#triumph-roll').val("");
+  };
+  
+  var resetChar = function () {
     $('#char-name').val("");
     $('#wound-thr').val("");
     $('#strain-thr').val("");
@@ -62,6 +72,45 @@ $(document).ready(function () {
       }
     }
   }
+  
+  Tracker.prototype.addPC = function () {
+    var success = document.getElementById("success-roll").value;
+    var advantage = document.getElementById("advantage-roll").value;
+    var triumph = document.getElementById("triumph-roll").value;
+
+    if (success > 0 || advantage > 0 || triumph > 0) {
+      init.slots.push({ type: 3, success: success, advantage: advantage, triumph: triumph, boost: 0, setback: 0, active: false, finished: false });
+    }
+
+    resetInit();
+    init.renderHTML();
+  }
+  
+  Tracker.prototype.addAlly = function () {
+    var success = document.getElementById("success-roll").value;
+    var advantage = document.getElementById("advantage-roll").value;
+    var triumph = document.getElementById("triumph-roll").value;
+
+    if (success > 0 || advantage > 0 || triumph > 0) {
+      init.slots.push({ type: 2, success: success, advantage: advantage, triumph: triumph, boost: 0, setback: 0, active: false, finished: false });
+    }
+
+    resetInit();
+    init.renderHTML();
+  }
+  
+  Tracker.prototype.addEnemy = function () {
+    var success = document.getElementById("success-roll").value;
+    var advantage = document.getElementById("advantage-roll").value;
+    var triumph = document.getElementById("triumph-roll").value;
+
+    if (success > 0 || advantage > 0 || triumph > 0) {
+      init.slots.push({ type: 1, success: success, advantage: advantage, triumph: triumph, boost: 0, setback: 0, active: false, finished: false });
+    }
+
+    resetInit();
+    init.renderHTML();
+  }
 
   Tracker.prototype.beginCombat = function () {
     for (var i = 0; i < this.slots.length; i++) {
@@ -84,7 +133,6 @@ $(document).ready(function () {
     currentSlot.setback = 0;
     this.activeIndex++;
     if (this.activeIndex > this.slots.length - 1) {
-      this.activeIndex = 0;
       this.endRound();
     } else {
       this.beginTurn();
@@ -98,6 +146,7 @@ $(document).ready(function () {
   }
   
   Tracker.prototype.endRound = function () {
+    this.activeIndex = 0;
     var currentSlot = this.slots[this.activeIndex];
     for (var i = 0; i < this.slots.length; i++) {
       var el = this.slots[i];
@@ -123,7 +172,6 @@ $(document).ready(function () {
     $(".tracker-list").empty();
     for (var i = 0; i < init.slots.length; i++) {
       var el = init.slots[i];
-      var roundCount = init.rounds;
       var htmlString = '<li class="slot';
       if (el.active === true) {
         htmlString += ' active-slot">';
@@ -158,44 +206,20 @@ $(document).ready(function () {
   
   //Adding a PC Slot.
   $(document).on("click", ".add-pc", function () {
-    var success = document.getElementById("success-roll").value;
-    var advantage = document.getElementById("advantage-roll").value;
-    var triumph = document.getElementById("triumph-roll").value;
-
-    if (success > 0 || advantage > 0 || triumph > 0) {
-      init.slots.push({ type: 3, success: success, advantage: advantage, triumph: triumph, boost: 0, setback: 0, active: false, finished: false });
-    }
-
-    reset();
-    init.renderHTML();
+    init.addPC();
+    display.addChar();
   });
   
   //Adding an Ally slot.
   $(document).on("click", ".add-ally", function () {
-    var success = document.getElementById("success-roll").value;
-    var advantage = document.getElementById("advantage-roll").value;
-    var triumph = document.getElementById("triumph-roll").value;
-
-    if (success > 0 || advantage > 0 || triumph > 0) {
-      init.slots.push({ type: 2, success: success, advantage: advantage, triumph: triumph, boost: 0, setback: 0, active: false, finished: false });
-    }
-
-    reset();
-    init.renderHTML();
+    init.addAlly();
+    display.addChar();
   });
   
   //Adding an Enemy slot.
   $(document).on("click", ".add-enemy", function () {
-    var success = document.getElementById("success-roll").value;
-    var advantage = document.getElementById("advantage-roll").value;
-    var triumph = document.getElementById("triumph-roll").value;
-
-    if (success > 0 || advantage > 0 || triumph > 0) {
-      init.slots.push({ type: 1, success: success, advantage: advantage, triumph: triumph, boost: 0, setback: 0, active: false, finished: false });
-    }
-
-    reset();
-    init.renderHTML();
+    init.addEnemy();
+    display.addChar();
   });
 
   $("#begin-combat").click(function () {
@@ -208,10 +232,6 @@ $(document).ready(function () {
   
   $("#end-round").click(function () {
     init.endRound();
-  })
-  
-  $("#end-combat").click(function () {
-    
   })
   
   $("#init-clear").click(function () {
@@ -230,9 +250,18 @@ $(document).ready(function () {
   
   $(document).on("click", ".setback-count", function() {
     var parentSlot = $(this).closest("li");
-    var boostObject = $(".tracker-list li").index(parentSlot);
+    var setbackObject = $(".tracker-list li").index(parentSlot);
     
-    init.slots[boostObject].setback++;
+    init.slots[setbackObject].setback++;
+    init.renderHTML();
+  })
+  
+  $(document).on("click", ".remove", function() {
+    var parentSlot = $(this).closest("li");
+    var slotObject = $(".tracker-list li").index(parentSlot);
+    
+    init.slots.splice(slotObject, 1);
+    init.slots.sort(compare);
     init.renderHTML();
   })
   
@@ -240,25 +269,156 @@ $(document).ready(function () {
   /* ========== CHARACTER MONITOR =========== */
   /* ======================================== */
   
-  //Declare Characters for the character monitor.
-  function Character(name, woundThr, strainThr) {
-    this.name = name;
-    this.woundThr = woundThr;
-    this.strainThr = strainThr;
-    this.boost = 0;
-    this.setback = 0;
-    this.crits = [];
-  }
+  //Set the selected character for manipulation.
+  // $(document).on("click", ".character", function () {
+  //   $(".character").removeClass("selected-char");
+  //   $(this).addClass("selected-char");
+  // });
+  
+  Monitor.prototype.addChar = function () {
+    var name = document.getElementById("char-name").value;
+    var wounds = document.getElementById("wound-thr").value;
+    var strain = document.getElementById("strain-thr").value;
 
-  function Crit(name, severity) {
-    this.name = name;
-    this.severity = severity;
+    if (name != 0 && wounds > 0) {
+      display.chars.push({ name: name, woundCurrent: 0, woundThr: wounds, strainCurrent: 0, strainThr: strain, boost: 0, setback: 0 });
+    }
+    
+    resetChar();
+    display.renderHTML();
   }
   
-  //Set the selected character for manipulation.
-  $(document).on("click", ".character", function () {
-    $(".character").removeClass("selected-char");
-    $(this).addClass("selected-char");
-  });
+  Monitor.prototype.renderHTML = function () {
+    $("#monitor-container").empty();
+    for (var i = 0; i < display.chars.length; i++) {
+      var el = display.chars[i];
+      var htmlString = '<div class="character">';
+      htmlString += '<h3>' + el.name + '</h3>'
+      htmlString += '<div class="stat-small-wide"><div class="tag-small';
+      if (el.woundCurrent >= el.woundThr) {
+        htmlString += ' tagRed';
+      }
+      htmlString += '">Wounds / Threshold</div>';
+      htmlString += '<div class="value-small">';
+      htmlString += '<div class="char-wound-dmg"><span class="icon icon-plus"></span></div> ';
+      htmlString += el.woundCurrent;
+      htmlString += ' / ';
+      htmlString += el.woundThr;
+      htmlString += ' <div class="char-wound-heal"><span class="icon icon-minus"></span></div>';
+      htmlString += '</div></div> ';
+      if (el.strainThr > 0) {
+        htmlString += '<div class="stat-small-wide">';
+        htmlString += '<div class="tag-small';
+        if (el.strainCurrent >= el.strainThr) {
+          htmlString += ' tagRed';
+        }
+        htmlString += '">Strain / Threshold</div>';
+        htmlString += '<div class="value-small">';
+        htmlString += '<div class="char-strain-dmg"><span class="icon icon-plus"></span></div> ';
+        htmlString += el.strainCurrent;
+        htmlString += ' / ';
+        htmlString += el.strainThr;
+        htmlString += ' <div class="char-strain-heal"><span class="icon icon-minus"></span></div>';
+        htmlString += '</div></div>';
+      }
+      htmlString += '<div class="char-field boost-count-char">'+ el.boost +'<span class="eote bo2"></span></div>';
+      htmlString += '<div class="char-field remove-dice remove-boost"><span class="icon icon-cross"></span></div>';
+      htmlString += '<div class="char-field setback-count-char">'+ el.setback +'<span class="eote se2 contrast"></span></div>';
+      htmlString += '<div class="char-field remove-dice remove-setback"><span class="icon icon-cross"></span></div>';
+      htmlString += '<div class="char-field right"><span class="icon icon-cross remove-char"></span></div></div>';
 
+      $("#monitor-container").append(htmlString);
+    }
+  }
+  
+  Monitor.prototype.clear = function () {
+    display.chars.length = 0;
+    display.renderHTML();
+  }
+  
+  $(document).on("click", ".char-wound-dmg", function () {
+    var parentSlot = $(this).closest(".character");
+    var woundObject = $("#monitor-container div.character").index(parentSlot);
+    
+    display.chars[woundObject].woundCurrent++;
+    display.renderHTML();
+  });
+  
+  $(document).on("click", ".char-wound-heal", function () {
+    var parentSlot = $(this).closest(".character");
+    var woundObject = $("#monitor-container div.character").index(parentSlot);
+    
+    display.chars[woundObject].woundCurrent--;
+    if (display.chars[woundObject].woundCurrent < 0) {
+      display.chars[woundObject].woundCurrent = 0;
+    }
+    display.renderHTML();
+  });
+  
+  $(document).on("click", ".char-strain-dmg", function () {
+    var parentSlot = $(this).closest(".character");
+    var woundObject = $("#monitor-container div.character").index(parentSlot);
+    
+    display.chars[woundObject].strainCurrent++;
+    if (display.chars[woundObject].strainCurrent > display.chars[woundObject].strainThr) {
+      display.chars[woundObject].strainCurrent = display.chars[woundObject].strainThr;
+    }
+    display.renderHTML();
+  });
+  
+  $(document).on("click", ".char-strain-heal", function () {
+    var parentSlot = $(this).closest(".character");
+    var woundObject = $("#monitor-container div.character").index(parentSlot);
+    
+    display.chars[woundObject].strainCurrent--;
+    if (display.chars[woundObject].strainCurrent < 0) {
+      display.chars[woundObject].strainCurrent = 0;
+    }
+    display.renderHTML();
+  });
+  
+  $(document).on("click", ".boost-count-char", function () {
+    var parentSlot = $(this).closest(".character");
+    var woundObject = $("#monitor-container div.character").index(parentSlot);
+    
+    display.chars[woundObject].boost++;
+    display.renderHTML();
+  });
+  
+  $(document).on("click", ".remove-boost", function () {
+    var parentSlot = $(this).closest(".character");
+    var woundObject = $("#monitor-container div.character").index(parentSlot);
+    
+    display.chars[woundObject].boost = 0;
+    display.renderHTML();
+  });
+  
+  $(document).on("click", ".setback-count-char", function () {
+    var parentSlot = $(this).closest(".character");
+    var woundObject = $("#monitor-container div.character").index(parentSlot);
+    
+    display.chars[woundObject].setback++;
+    display.renderHTML();
+  });
+  
+  $(document).on("click", ".remove-setback", function () {
+    var parentSlot = $(this).closest(".character");
+    var woundObject = $("#monitor-container div.character").index(parentSlot);
+    
+    display.chars[woundObject].setback = 0;
+    display.renderHTML();
+  });
+  
+  $(document).on("click", ".remove-char", function () {
+    var parentSlot = $(this).closest(".character");
+    var woundObject = $("#monitor-container div.character").index(parentSlot);
+    
+    display.chars.splice(woundObject, 1);
+    display.renderHTML();
+  });
+  
+  $("#char-clear").click(function() {
+    display.clear();
+  });
+  
 });
